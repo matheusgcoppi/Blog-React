a3a21d491e8d583d54797f9e413203368cb432d4
Matheus Coppi <matheuscoppi22@gmail.com>
2023-01-24 21:42:25 -0300

    Secondcommit

--- src/App.js
+++ src/App.js
@@ -2,17 +2,22 @@ import './App.css';
 import React from 'react'
 import Navbar from './Navbar';
 import Home from './Home';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Create from './Create';
 
 function App() {
-  
-  
   return (
-    <div className="App">
-      <Navbar></Navbar>
-      <div className="content">
-        <Home></Home>
+    <Router>
+      <div className="App">
+        <Navbar></Navbar>
+        <div className="content">
+          <Routes>
+            <Route exact path= "/" element= {<Home/>}></Route>
+            <Route path= "/create" element= {<Create/>}></Route>
+            </Routes>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
